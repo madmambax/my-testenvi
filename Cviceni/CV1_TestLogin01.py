071c88fe389c62e1bba4afdbeccc9fb73945876c
radek <radek@kitner.cz>
2021-05-19 14:03:21 +0200

    přidáno cvičení 01

--- Cviceni/CV1_TestLogin01.py
+++ Cviceni/CV1_TestLogin01.py
@@ -1,1 +1,37 @@
+*** Settings ***
+Library	Collections
+Library	RequestsLibrary
+
+
+*** Variables ***
+${url}		http://testovani.kitner.cz/
+
+
+*** Test Cases ***
+Login
+    [Documentation]  	Uspesne prihlaseni
+    Login   novak       tajnenovak  200
+
+
+
+
+*** Keywords ***
+
+
+Login
+    [Arguments]    ${username}    ${password}   ${responce_code}
+    ${json_string}=     catenate  {"username":"${username}","password":"${password}","useragent":"Chrome"}
+    &{header}=    Create Dictionary    Content-Type=application/json
+    CreateSession    apilogin    ${url}
+    ${resp} =    Post Request    apilogin    login_app/userauth.php  data=${json_string}  headers=${header}
+    Log	Responce: @{resp}
+    Should Be Equal As Strings	${resp.status_code}     200
+    Dictionary Should Contain Key	${resp.json()}      response
+    Dictionary Should Contain Value	${resp.json()}      ${responce_code}
+    
+
+
+
+
+
  
