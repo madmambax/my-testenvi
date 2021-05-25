0b29e0f8cd9c832a6c6a05a99634a2c15e5c87ae
CPatricie <trice@seznam.cz>
2021-05-25 21:16:15 +0200

    doplneni kalkulacky o nasobeni

---
+++ Ucastnici/Patricie Cizova/01_kalkulacka.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Ukázka využití funkce při programování
+
+# definujeme novou funkci pro sčítání
+def add(x,y):
+    return x+y
+
+#zavoláme funkci s parametry 5 a 7
+res = add(5,7)
+
+
+### Úkol: zde přidejte novou funkci pro násobení !!! ###
+#?
+def nas(a,b,c):
+    return a*b*c
+
+vys = nas(5,7,9)
+
+#vypíšeme výsledek
+print("Vysledek scitani: ", res)
+print("Vysledek nasobeni: ",vys)
