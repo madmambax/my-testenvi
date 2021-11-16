b50590d71f36c0f9df016a588c110421751b64cf
radek <radek@kitner.cz>
2021-11-16 16:29:25 +0100

    fce v pythonu

---
+++ Bonusy/Funkce v Pythonu/01_kalkulacka.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Ukázka využití funkce při programování
+
+# definujeme novou funkci pro sčítání
+def add(x,y):
+    return x+y
+
+#zavoláme funkci s parametry 5 a 7
+res = add(5,7)
+
+
+### Úkol: zde přidejte novou funkci pro násobení !!! ###
+#?
+
+#vypíšemy výsledek
+print(res)
+
