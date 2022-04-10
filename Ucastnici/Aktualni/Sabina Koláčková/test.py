bbabfb2f50a08e0c1c140a9ae7ef135216a389b0
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-04-10 12:01:41 +0200

    Přidání testu

---
+++ Ucastnici/Aktualni/Sabina Koláčková/test.py
@@ -0,0 +1,16 @@
+*** Settings ***
+Library     OperatingSystem
+
+
+*** Test Cases ***
+
+Prvni test pozitivní
+    ${res} =        run     help dir
+    log             ${res}
+    Should Contain  ${res}  For more information on a specific command, type HELP command-name
+
+
+Druhy test negativni
+    ${res} =        run     help ahoj
+    log             ${res}
+    Should Contain  ${res}  This command is not supported by the help utility.
